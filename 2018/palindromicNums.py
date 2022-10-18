79eb2364caae9cc7c849506451bfe935db5f535f
Stefan Nedelcu <stefan.n@gmx.co.uk>
2022-10-18 21:06:01 +0100

    started to implement full mark solution

--- 2018/palindromicNums.py
+++ 2018/palindromicNums.py
@@ -25,10 +25,25 @@ class Palindrome:
     def isPalindrome2(self):
         return str(self.num)[::-1] == str(self.num)
 
-    def getNearestPal(self):
-        pass
+    def getPalSmart(self, num, headP=0):
+        '''
+        num - list of ints
+        get smaller next front half and then put it onto the last half in reverse
+        e.g. 17 -> next smallest front half is 2. put 2 into second half ---> 22
+        '''
+        # print(num)
+        if headP >= len(num) // 2:
+            print(''.join([str(i) for i in num]))
+            return
+        if num[headP] != num[-1 - headP] and num[headP] > num[-1 - headP]:
+            num[-1 - headP] = num[headP]
+        elif num[headP] != num[-1 - headP] and num[-1 - headP] > num[headP]:
+            num[headP] += 1  # e.g. 17 --> 22
+            num[-1 - headP] = num[headP]
 
-    def getPal(self):
+        self.getPalSmart(num, headP + 1)
+
+    def getPalNaive(self):
         while self.isPalindrome2() == False:
             self.num += 1
         return self.num
@@ -36,14 +51,19 @@ class Palindrome:
 def checkPal():
     num = int(input())
     pal = Palindrome(num)
+    # dumb version
     start = time.time()
-    res = pal.getPal()
+    res = pal.getPalNaive()
     end = time.time()
     print(res, end - start)
+    # smarter version
+    num += 1
+    num = [int(x) for x in str(num)]
+    pal.getPalSmart(num)
 
 def main():
-    # checkPal()
-    pass
+    checkPal()
+    #pass
 
 if __name__ == '__main__':
     main()
