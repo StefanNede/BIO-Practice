b0e100107c5c28ef3e2e7a16fa1c7f7b4e856c3c
Stefan Nedelcu <stefan.n@gmx.co.uk>
2022-11-10 19:07:48 +0000

    loses 6 marks

---
+++ 2009/childPlay.py
@@ -0,0 +1,23 @@
+# BIO round 1 q3 - 17/23
+from functools import lru_cache 
+import time
+
+res = []
+
+@lru_cache(maxsize=None)
+def solve(s, nums):
+    # dfs solution
+    global res
+    if s == 0:
+        res.append(nums)
+        return
+    else:
+        for i in range(1, min(s, 9)+1):
+            solve(s-i, nums+str(i))
+
+s, n = [int(i) for i in input().split(' ')]
+start = time.time()
+solve(s, '')
+end = time.time()
+print(' '.join(res[n-1]))
+print(end-start)
