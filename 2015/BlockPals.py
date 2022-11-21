1fac265133a80426ebac1671c34ec1092ecc765c
Stefan Nedelcu <stefan.n@gmx.co.uk>
2022-11-21 17:15:36 +0000

    full mark solution added

--- 2015/BlockPals.py
+++ 2015/BlockPals.py
@@ -1,7 +1,13 @@
-# Round 1 2015 q1
+# Round 1 2015 q1 - 23/23
 
+count = 0
 def solve(word:str) -> int:
-    return 0
+    global count
+    for i in range(1, len(word)//2 + 1):
+        if word[:i] == word[len(word)-i:]:
+            count += 1
+            solve(word[i:len(word)-i])
 
 word = input()
-print(solve(word))
+solve(word)
+print(count)
