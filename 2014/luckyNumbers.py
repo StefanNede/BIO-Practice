bb1ed5d179a675ec0ebbb35bdb8283ae6df43534
Stefan Nedelcu <stefan.n@gmx.co.uk>
2022-11-24 21:37:22 +0000

    full mark solution added

---
+++ 2014/luckyNumbers.py
@@ -0,0 +1,27 @@
+# BIO round 1 q1 - 25/25
+# longest test case takes 1.02 seconds but for this paper it says 2 seconds is the limit
+
+def solve(n):
+    odds = [1]
+    for i in range(n*2): odds.append(odds[-1] + 2)
+
+    previousLucky = 0
+    index = 1 # to ignore the 1
+    while True:
+        curr = odds[index]
+
+        if curr > n:
+            return previousLucky, curr
+
+        inter = [i for i in odds]
+        for i in range(len(inter)):
+            if (i+1)%curr == 0:
+                odds.remove(inter[i])
+        
+        if curr != n:
+            previousLucky = curr
+        index += 1
+                
+n = int(input())
+res = solve(n)
+print(f"{res[0]} {res[1]}")
