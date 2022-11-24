c02eb327d985e81d4861dea9540cb40e535428ac
Stefan Nedelcu <stefan.n@gmx.co.uk>
2022-11-24 20:39:09 +0000

    full mark solution added

--- 2016/migration.py
+++ 2016/migration.py
@@ -1,7 +1,4 @@
-# BIO round 1 q2 - 11/24
-# need to do:
-# - get central 5x5 board for output
-# - fix addArr
+# BIO round 1 q2 - 24/24
 
 class Migration:
     def __init__(self, startPos, sequVals):
@@ -12,30 +9,24 @@ class Migration:
         self.board = [[0, 0, 0, 0, 0] for i in range(5)] # to start have a 5x5 board - as startPos is in range 1 to 25
     
     def getCoor(self, pos):
+        # gets the coordinate within a 5x5 grid
         # outputs the indexes need to access a 2d array from self.position
-        boardLength = len(self.board)
-        xCoor = pos//boardLength
-        yCoor = pos - xCoor*boardLength
+        xCoor = pos//5
+        yCoor = pos - xCoor*5
         return [xCoor, yCoor]
 
     def addArr(self):
         # add above, below, left and right to keep the board a square
         subArrayLength = len(self.board[0])
-        subArray = [0] * subArrayLength
+        subArray = [0]*subArrayLength
         # first add vertical
         self.board = [subArray] + self.board
         self.board.append(subArray)
         # then horizontal
-        for subArray in self.board:
-            subArray.insert(0,0)
-            subArray.append(0)
+        for i in range(len(self.board)):
+            sub = [0] + self.board[i] + [0]
+            self.board[i] = sub
         
-        # update self.position
-        positionX, positionY = self.getCoor(self.position)
-        positionX += 1
-        positionY += 1
-        self.position = len(self.board[0])*positionX + positionY
-    
     def migrate(self, coor):
         # moves people out of the current square
         # run checks to enlarge grid
@@ -43,7 +34,7 @@ class Migration:
             coor[0] += 1
             coor[1] += 1
             self.addArr()
-        if coor[1]+1 > len(self.board[0]):
+        if coor[1]+1 >= len(self.board[0]):
             coor[0] += 1
             coor[1] += 1
             self.addArr()
@@ -51,7 +42,7 @@ class Migration:
             coor[0] += 1
             coor[1] += 1
             self.addArr()
-        if coor[0]+1 > len(self.board):
+        if coor[0]+1 >= len(self.board):
             coor[0] += 1
             coor[1] += 1
             self.addArr()
@@ -74,9 +65,10 @@ class Migration:
         # simulate next move
         if not self.firstMove:
             self.position += self.sequVals[self.currentSequValIndex]
-            self.position %= self.getTotalBoardSize()
+            self.position %= 25
+        # moves will only be within the 5x5 grid so we need to do this
         currentX, currentY = self.getCoor(self.position)
-        self.board[currentX][currentY] += 1
+        self.board[(len(self.board)-5)//2 + currentX][(len(self.board)-5)//2 + currentY] += 1
 
         # migrate people out
         # could optimise by checking if the squares people migrate will exceed 3 after the migration
@@ -99,7 +91,12 @@ class Migration:
 
     def outputBoard(self):
         # make sure to only output the central 5x5 board
-        for row in self.board:
+        outputBoard = []
+        for i in range((len(self.board)-5)//2, len(self.board) - (len(self.board)-5)//2):
+            b = self.board[i]
+            outputBoard.append(b[(len(self.board)-5)//2: len(self.board) - (len(self.board)-5)//2])
+        
+        for row in outputBoard:
             for number in row:
                 print(number, end=" ")
             print()
