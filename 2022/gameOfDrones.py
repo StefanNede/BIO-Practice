e2d21e4f442b72e7d1fe6ed4362fdcbeb312d08c
Stefan Nedelcu <stefan.n@gmx.co.uk>
2022-12-04 11:50:23 +0000

    partial solution added

--- 2022/gameOfDrones.py
+++ 2022/gameOfDrones.py
@@ -1,57 +1,106 @@
-# BIO round 1 2022 q2
-
-# Expected difficulties:
-# - changing who owns an edge depending on what hexagons share that edge
-#   e.g. red takes ownershiop of edge 3 of hexagon 19 which was previously owned by blue as that is edge 6 of hexagon 25 
-#       - do this sort of cleaning by, when setting an edge, also look at all touching edges
+# BIO round 1 2022 q2 - 11/27
+# skirmishes working
+# feud not started
 
 class Game:
-    hexagon = ['','','','','','']
     def __init__(self, rJump, bJump):
         self.rJump = rJump
         self.bJump = bJump
         self.rCount = 0
         self.bCount = 0
-        self.hexGrid = [[self.hexagon] * 25]
-
-    def changeHexGrid(self, colony:str, controlled:[int]):
-        '''
-        colony - (r)ed or (b)lue
-        controlled - [hexNum, hexEdge]
-        General rules for adjacent hex edges:
-        x,1 = y,4        
-        x,2 = y,5
-        x,3 = y,6
-        x,4 = y,1
-        x,5 = y,2
-        x,6 = y,3
-
-        Now to find link between x and y:
-
-        '''
-        # do changes here 
-        hexNum, hexEdge = controlled
-        self.hexGrid[hexNum][hexEdge] = colony
-        if hexEdge <= 3:
-            self.hexGrid[y][hexEdge+3] = colony
-        elif hexEdge >= 4:
-            self.hexGrid[y][hexEdge-3] = colony
+        self.rEdge = [0,0] # starts facing edge 1 on the first hexagon
+        self.bEdge = [5, 24] # starts facing edge 6 on the last hexagon
+        self.grid = [["" for i in range(6)] for i in range(25)] # hold who is in charge overall
     
-    def skirmish(self):
-        '''
-        red drone:
-        - takes ownership of edge it's facing
-        - rotates 60 degrees clockwise 
-        - jumps r hexagons along the hive
+    def controlsHexagon(self):
+        self.rCount = 0
+        self.bCount = 0
+        # gets the score for red and blue by looping over the hexagons and seeing who has more edges
+        for hexagon in self.grid:
+            if hexagon.count("r") > hexagon.count("b"):
+                self.rCount += 1
+            elif hexagon.count("b") > hexagon.count("r"):
+                self.bCount += 1
 
-        blue drone:
-        - takes ownership of edge it's facing
-        - rotates 60 degrees anti-clockwise 
-        - jumps b hexagons along the hive
-        '''
-        pass
+    def takeOwnership(self, drone, edge, hexagon):
+        '''changes the edge in the current hexagon and any bordering hexagons'''
+        # change current hexagon
+        self.grid[hexagon][edge] = drone
 
-    def feud(self):
+        # change adjacent coordinate
+        # handle no adjacent coordinates
+        if (hexagon <= 4) and (edge == 0 or edge == 5): return 
+        elif (hexagon == 0 or hexagon == 10 or hexagon == 20) and (edge >= 3): return  
+        elif (hexagon == 4) and (edge==5 or edge == 0 or edge == 1): return 
+        elif (hexagon == 9 or hexagon == 19) and (edge<=2): return
+        elif (hexagon == 5 or hexagon == 15) and edge == 4: return 
+        elif (hexagon == 14 or hexagon == 24) and edge == 1: return
+        elif (hexagon >= 20) and (edge == 2 or edge == 3): return
+        # handle adjacent coordinates
+        # adjacent to the right
+        elif edge == 1:
+            self.grid[hexagon+1][4] = drone
+        # adjacent to the left
+        elif edge == 4:
+            self.grid[hexagon-1][1] = drone
+        # adjacent to the top right
+        elif edge == 0:
+            resHexa = 0
+            if (hexagon >= 5 and hexagon <= 9) or (hexagon >= 15 and hexagon <= 19):
+                resHexa = hexagon - 4
+            else:
+                resHexa = hexagon - 5
+            self.grid[resHexa][3] = drone
+        # adjacent to the top left
+        elif edge == 5:
+            resHexa = 0
+            if (hexagon >= 5 and hexagon <= 9) or (hexagon >= 15 and hexagon <= 19):
+                resHexa = hexagon - 5
+            else:
+                resHexa = hexagon - 6
+            self.grid[resHexa][2] = drone
+        # adjacent to the bottom right
+        elif edge == 2:
+            resHexa = 0
+            if (hexagon >= 5 and hexagon <= 9) or (hexagon >= 15 and hexagon <= 19):
+                resHexa = hexagon + 6
+            else:
+                resHexa = hexagon + 5
+            self.grid[resHexa][5] = drone
+        # adjacent to the bottom left
+        elif edge == 3:
+            resHexa = 0
+            if (hexagon >= 5 and hexagon <= 9) or (hexagon >= 15 and hexagon <= 19):
+                resHexa = hexagon + 5
+            else:
+                resHexa = hexagon + 4
+            self.grid[resHexa][0] = drone
+
+    def skirmish(self, drone):
+        if drone == "r":
+            # take ownership of edge it is facing 
+            edge, hexa = self.rEdge
+            self.takeOwnership(drone, edge, hexa)
+            # rotate 60 degree clockwise
+            edge += 1
+            edge %= 6
+            # jump r hexagons along hive
+            hexa += self.rJump
+            hexa %= 25
+            self.rEdge = [edge, hexa]
+        else:
+            # take ownership of edge it is facing 
+            edge, hexa = self.bEdge
+            self.takeOwnership(drone, edge, hexa)
+            # rotate 60 degree anticlockwise
+            edge -= 1
+            edge %= 6
+            # jump b hexagons along hive
+            hexa += self.bJump
+            hexa %= 25
+            self.bEdge = [edge, hexa]
+        
+    def feud(self, drone):
         '''
         red then blue:
         - take ownership of preferred un-owned edge
@@ -66,10 +115,15 @@ def main():
     rJump, bJump = [int(i) for i in input().split(' ')]
     skirmishes, feuds = [int(i) for i in input().split(' ')] 
     g = Game(rJump, bJump)
-    for i in range(skirmishes): g.skirmish()
-    for j in range(feuds): g.feud()
+    for i in range(skirmishes): 
+        g.skirmish("r")
+        g.skirmish("b")
+    for j in range(feuds): 
+        g.feud("r")
+        g.feud("b")
+    # print(g.grid)
+    g.controlsHexagon()
     print(g.rCount)
     print(g.bCount)
 
-if __name__ == "__main__":
-    main()
+main()
