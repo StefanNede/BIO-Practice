f48c2592022cfce590790d8554d7c3754961a011
Stefan Nedelcu <stefan.n@gmx.co.uk>
2022-12-04 13:01:54 +0000

    template for feuds set up

--- 2022/gameOfDrones.py
+++ 2022/gameOfDrones.py
@@ -13,36 +13,38 @@ class Game:
         self.grid = [["" for i in range(6)] for i in range(25)] # hold who is in charge overall
     
     def controlsHexagon(self):
-        self.rCount = 0
-        self.bCount = 0
+        interRCount = 0
+        interBCount = 0
         # gets the score for red and blue by looping over the hexagons and seeing who has more edges
         for hexagon in self.grid:
             if hexagon.count("r") > hexagon.count("b"):
-                self.rCount += 1
+                interRCount += 1
             elif hexagon.count("b") > hexagon.count("r"):
-                self.bCount += 1
+                interBCount += 1
+        return [interRCount, interBCount]
 
     def takeOwnership(self, drone, edge, hexagon):
         '''changes the edge in the current hexagon and any bordering hexagons'''
         # change current hexagon
-        self.grid[hexagon][edge] = drone
+        interGrid = self.grid
+        interGrid[hexagon][edge] = drone
 
         # change adjacent coordinate
         # handle no adjacent coordinates
-        if (hexagon <= 4) and (edge == 0 or edge == 5): return 
-        elif (hexagon == 0 or hexagon == 10 or hexagon == 20) and (edge >= 3): return  
-        elif (hexagon == 4) and (edge==5 or edge == 0 or edge == 1): return 
-        elif (hexagon == 9 or hexagon == 19) and (edge<=2): return
-        elif (hexagon == 5 or hexagon == 15) and edge == 4: return 
-        elif (hexagon == 14 or hexagon == 24) and edge == 1: return
-        elif (hexagon >= 20) and (edge == 2 or edge == 3): return
+        if (hexagon <= 4) and (edge == 0 or edge == 5): return interGrid
+        elif (hexagon == 0 or hexagon == 10 or hexagon == 20) and (edge >= 3): return interGrid
+        elif (hexagon == 4) and (edge==5 or edge == 0 or edge == 1): return interGrid
+        elif (hexagon == 9 or hexagon == 19) and (edge<=2): return interGrid
+        elif (hexagon == 5 or hexagon == 15) and edge == 4: return  interGrid
+        elif (hexagon == 14 or hexagon == 24) and edge == 1: return interGrid
+        elif (hexagon >= 20) and (edge == 2 or edge == 3): return interGrid
         # handle adjacent coordinates
         # adjacent to the right
         elif edge == 1:
-            self.grid[hexagon+1][4] = drone
+            interGrid[hexagon+1][4] = drone
         # adjacent to the left
         elif edge == 4:
-            self.grid[hexagon-1][1] = drone
+            interGrid[hexagon-1][1] = drone
         # adjacent to the top right
         elif edge == 0:
             resHexa = 0
@@ -50,7 +52,7 @@ class Game:
                 resHexa = hexagon - 4
             else:
                 resHexa = hexagon - 5
-            self.grid[resHexa][3] = drone
+            interGrid[resHexa][3] = drone
         # adjacent to the top left
         elif edge == 5:
             resHexa = 0
@@ -58,7 +60,7 @@ class Game:
                 resHexa = hexagon - 5
             else:
                 resHexa = hexagon - 6
-            self.grid[resHexa][2] = drone
+            interGrid[resHexa][2] = drone
         # adjacent to the bottom right
         elif edge == 2:
             resHexa = 0
@@ -66,7 +68,7 @@ class Game:
                 resHexa = hexagon + 6
             else:
                 resHexa = hexagon + 5
-            self.grid[resHexa][5] = drone
+            interGrid[resHexa][5] = drone
         # adjacent to the bottom left
         elif edge == 3:
             resHexa = 0
@@ -74,13 +76,15 @@ class Game:
                 resHexa = hexagon + 5
             else:
                 resHexa = hexagon + 4
-            self.grid[resHexa][0] = drone
+            interGrid[resHexa][0] = drone
+        return interGrid
 
     def skirmish(self, drone):
         if drone == "r":
             # take ownership of edge it is facing 
             edge, hexa = self.rEdge
-            self.takeOwnership(drone, edge, hexa)
+            self.grid = self.takeOwnership(drone, edge, hexa)
+            
             # rotate 60 degree clockwise
             edge += 1
             edge %= 6
@@ -91,7 +95,7 @@ class Game:
         else:
             # take ownership of edge it is facing 
             edge, hexa = self.bEdge
-            self.takeOwnership(drone, edge, hexa)
+            self.grid = self.takeOwnership(drone, edge, hexa)
             # rotate 60 degree anticlockwise
             edge -= 1
             edge %= 6
@@ -99,17 +103,49 @@ class Game:
             hexa += self.bJump
             hexa %= 25
             self.bEdge = [edge, hexa]
+    
+    def beforeFeud(self):
+        # sets stuff up before feuds take place
+        self.rCount, self.bCount = self.controlsHexagon()
         
     def feud(self, drone):
+        # might need to modify takeOwnership to give an intermittent grid
+        # as we are not always going to choose that grid
         '''
-        red then blue:
-        - take ownership of preferred un-owned edge
+        take ownership of preferred un-owned edge
             - the edge that gains them control over the most hexagons
             - if edges give them the same amount of control choose the one that takes away most control from other colony
             - if still undecided - red choose lowest hexagon number, blue highest
             - if still undecided - red choose lowest direction number, blue highest
         '''
-        pass
+        if drone == "r":
+            maxControlNum = 0
+            maxControlEdge = 0
+            maxControlHexagon = 0
+            # at most they can gain control over 2 hexagons from one edge
+            for i in range(len(self.grid)):
+                cHexagon = self.grid[i]
+                # check all the adjacent hexagons where an edge could be placed (loop over edges) and if  
+                # u place edges there red count is higher than blue count for both hexagons
+                # then set maxControlNum to 2 and store edge and hexagon
+                # if this gives control over 1 edge and maxControlNum is 0 then set it to that
+                # otherwise a smaller number hexagon gave them the same control so this is preferred
+                # if no control then see if number of red edges == blue edges meaning they took away from 
+                # other drones, and store this only if maxControlNum is 0
+                for edge in range(5):
+                    pass
+
+        else:
+            # same method as for red but instead of only storing if nothing has been stored previously
+            # because trying to get lowest hexagon number
+            # continue to store even if a smaller hexagon gives the same outcome
+            maxControlNum = 0
+            maxControlEdge = 0
+            maxControlHexagon = 0
+            for i in range(len(self.grid)):
+                cHexagon = self.grid[i]
+                for edge in range(5):
+                    pass
 
 def main():
     rJump, bJump = [int(i) for i in input().split(' ')]
@@ -118,11 +154,10 @@ def main():
     for i in range(skirmishes): 
         g.skirmish("r")
         g.skirmish("b")
+    g.beforeFeud()
     for j in range(feuds): 
         g.feud("r")
         g.feud("b")
-    # print(g.grid)
-    g.controlsHexagon()
     print(g.rCount)
     print(g.bCount)
 
